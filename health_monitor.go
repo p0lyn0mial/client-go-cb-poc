60e4b5f4c0917531c0f0c9f26135a2a50d96d1ff
Lukasz Szaszkiewicz <lukasz.szaszkiewicz@gmail.com>
2021-02-11 17:25:12 +0100

    health monitor, part2

--- health_monitor.go
+++ health_monitor.go
@@ -12,9 +12,15 @@ import (
 	"k8s.io/apimachinery/pkg/util/wait"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/transport"
+	"k8s.io/klog/v2"
 )
 
 type healthMonitor struct {
+	// targetProvider provides a list of targets to monitor
+	// it also can schedule refreshing the list by simply calling Enqueue method
+	targetProvider TargetProvider
+
+	// client is an HTTP client that is used to probe health checks for targets
 	client *http.Client
 
 	// probeResponseTimeout specifies a time limit for requests made by the HTTP client for the health check
@@ -35,10 +41,12 @@ type healthMonitor struct {
 
 	consecutiveSuccessfulProbes map[string]int
 	consecutiveFailedProbes     map[string][]error
+
+	refreshTargetsLock sync.Mutex
+	refreshTargets     bool
 }
 
-// TODO: move serverToMonitor to a provider that can return a dynamic list
-func NewHealthMonitor(serversToMonitor []string, restConfig *rest.Config) (*healthMonitor, error) {
+func NewHealthMonitor(targetProvider TargetProvider, restConfig *rest.Config) (*healthMonitor, error) {
 	probeResponseTimeout := 5 * time.Second
 
 	client, err := createHealthCheckHTTPClient(probeResponseTimeout, restConfig)
@@ -47,10 +55,11 @@ func NewHealthMonitor(serversToMonitor []string, restConfig *rest.Config) (*heal
 	}
 
 	return &healthMonitor{
-		targetsToMonitor:         serversToMonitor,
 		client:                   client,
+		targetProvider:           targetProvider,
+		targetsToMonitor:         targetProvider.CurrentTargetsList(),
 		probeResponseTimeout:     probeResponseTimeout,
-		probeInterval:            10 * time.Second,
+		probeInterval:            1 * time.Second,
 		unhealthyProbesThreshold: 2,
 		healthyProbesThreshold:   5,
 
@@ -59,12 +68,22 @@ func NewHealthMonitor(serversToMonitor []string, restConfig *rest.Config) (*heal
 	}, nil
 }
 
+// StartMonitoring starts monitoring the provided targets until stop channel is closed
 func (sm *healthMonitor) StartMonitoring(stopCh <-chan struct{}) {
 	defer utilruntime.HandleCrash()
 
-	// TODO: add logging
+	klog.Infof("Starting the health monitor with Interval = %v, Timeout = %v, HealthyThreshold = %v, UnhealthyThreshold = %v ", sm.probeInterval, sm.probeResponseTimeout, sm.healthyProbesThreshold, sm.unhealthyProbesThreshold)
+	defer klog.Info("Shutting down the health monitor")
 
-	go wait.Until(sm.healthCheckRegisteredTargets, sm.probeInterval, stopCh)
+	wait.Until(sm.healthCheckRegisteredTargets, sm.probeInterval, stopCh)
+}
+
+// Enqueue schedules refreshing the target list on the next probeInterval
+// This method is used by the TargetProvider to notify that the list has changed
+func (sm *healthMonitor) Enqueue() {
+	sm.refreshTargetsLock.Lock()
+	defer sm.refreshTargetsLock.Unlock()
+	sm.refreshTargets = true
 }
 
 type targetErrTuple struct {
@@ -72,21 +91,59 @@ type targetErrTuple struct {
 	err    error
 }
 
+// refreshTargetsLocked updates the internal targets list to monitor if it was requested (via the Enqueue method)
+func (sm *healthMonitor) refreshTargetsLocked() {
+	sm.refreshTargetsLock.Lock()
+	defer sm.refreshTargetsLock.Unlock()
+	if !sm.refreshTargets {
+		return
+	}
+
+	sm.refreshTargets = false
+	freshTargets := sm.targetProvider.CurrentTargetsList()
+	freshTargetSet := sets.NewString(freshTargets...)
+
+	currentTargetsSet := sets.NewString(sm.targetsToMonitor...)
+	newTargetsToMonitorSet := freshTargetSet.Difference(currentTargetsSet)
+	if newTargetsToMonitorSet.Len() > 0 {
+		klog.Infof("health monitor observed new targets = %v", newTargetsToMonitorSet.List())
+	}
+
+	removedTargetsToMonitorSet := currentTargetsSet.Difference(freshTargetSet)
+	if removedTargetsToMonitorSet.Len() > 0 {
+		klog.Infof("health monitor will stop checking the following targets targets = %v", removedTargetsToMonitorSet.List())
+		for targetToRemove, _ := range removedTargetsToMonitorSet {
+			delete(sm.consecutiveSuccessfulProbes, targetToRemove)
+			delete(sm.consecutiveFailedProbes, targetToRemove)
+		}
+
+		healthyTargetsSet := sets.NewString(sm.healthyTargets...)
+		healthyTargetsSet.Delete(removedTargetsToMonitorSet.List()...)
+		sm.healthyTargets = healthyTargetsSet.List()
+
+		unhealthyTargetsSet := sets.NewString(sm.unhealthyTargets...)
+		unhealthyTargetsSet.Delete(removedTargetsToMonitorSet.List()...)
+		sm.unhealthyTargets = unhealthyTargetsSet.List()
+	}
+
+	sm.targetsToMonitor = freshTargets
+}
+
 func (sm *healthMonitor) healthCheckRegisteredTargets() {
-	// TODO: check if the list of servers to monitors has changed
+	sm.refreshTargetsLocked()
 	var wg sync.WaitGroup
 	resTargetErrTupleCh := make(chan targetErrTuple, len(sm.targetsToMonitor))
 
 	for i := 0; i < len(sm.targetsToMonitor); i++ {
 		wg.Add(1)
-		go func(server string) {
+		go func(target string) {
 			defer wg.Done()
-			if err := sm.healthCheckSingleTarget(server); err != nil {
-				resTargetErrTupleCh <- targetErrTuple{server, err}
-			}
+			err := sm.healthCheckSingleTarget(target)
+			resTargetErrTupleCh <- targetErrTuple{target, err}
 		}(sm.targetsToMonitor[i])
 	}
 	wg.Wait()
+	close(resTargetErrTupleCh)
 
 	currentHealthCheckProbes := make([]targetErrTuple, 0, len(sm.targetsToMonitor))
 	for svrErrTuple := range resTargetErrTupleCh {
@@ -96,6 +153,10 @@ func (sm *healthMonitor) healthCheckRegisteredTargets() {
 	sm.updateHealthChecksFor(currentHealthCheckProbes)
 }
 
+// TODO: add metrics
+// updateHealthChecksFor examines the health of targets based on the provided probes and the current configuration.
+// It also notifies interested parties about changes in the health condition.
+// Interested parties can be registered by calling AddListener method.
 func (sm *healthMonitor) updateHealthChecksFor(currentHealthCheckProbes []targetErrTuple) {
 	newUnhealthyTargets := []string{}
 	newHealthyTargets := []string{}
@@ -133,27 +194,27 @@ func (sm *healthMonitor) updateHealthChecksFor(currentHealthCheckProbes []target
 	// detect unhealthy targets
 	previouslyUnhealthyTargetsSet := sets.NewString(sm.unhealthyTargets...)
 	currentlyUnhealthyTargetsSet := previouslyUnhealthyTargetsSet.Union(newUnhealthyTargetsSet)
-	currentlyUnhealthyTargetsSet = currentlyUnhealthyTargetsSet.Delete(newHealthyTargetsSet.List()...)
+	currentlyUnhealthyTargetsSet.Delete(newHealthyTargetsSet.List()...)
 	if !currentlyUnhealthyTargetsSet.Equal(previouslyUnhealthyTargetsSet) {
 		// TODO: notify about new unhealthy targets
-		// TODO: add metrics
 		sm.unhealthyTargets = currentlyUnhealthyTargetsSet.List()
+		klog.Infof("observed the following unhealthy targets %v", sm.unhealthyTargets)
 	}
 
 	// detect healthy targets
 	previouslyHealthyTargetsSet := sets.NewString(sm.healthyTargets...)
 	currentlyHealthyTargetsSet := previouslyHealthyTargetsSet.Union(newHealthyTargetsSet)
-	currentlyHealthyTargetsSet = currentlyHealthyTargetsSet.Delete(newUnhealthyTargetsSet.List()...)
+	currentlyHealthyTargetsSet.Delete(newUnhealthyTargetsSet.List()...)
 	if !currentlyHealthyTargetsSet.Equal(previouslyHealthyTargetsSet) {
 		// TODO: notify about new healthy servers
-		// TODO: add metrics
 		sm.healthyTargets = currentlyHealthyTargetsSet.List()
+		klog.Infof("observed the following healthy targets %v", sm.healthyTargets)
 	}
 }
 
 func (sm *healthMonitor) healthCheckSingleTarget(target string) error {
 	// TODO: make the protocol, port and the path configurable
-	url := fmt.Sprintf("https://%s/%s", target, "/readyz")
+	url := fmt.Sprintf("https://%s/%s", target, "readyz")
 	newReq, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		return err
--- health_monitor_test.go
+++ health_monitor_test.go
@@ -8,6 +8,190 @@ import (
 	"github.com/google/go-cmp/cmp/cmpopts"
 )
 
+type fakeTargetProvider []string
+
+func (sp fakeTargetProvider) CurrentTargetsList() []string {
+	return sp
+}
+
+func TestInternalStateAfterRefreshingTargets(t *testing.T) {
+	monitor := &healthMonitor{
+		unhealthyProbesThreshold: 2,
+		healthyProbesThreshold:   3,
+
+		consecutiveSuccessfulProbes: map[string]int{},
+		consecutiveFailedProbes:     map[string][]error{},
+	}
+
+	scenarios := []struct {
+		name                 string
+		shouldRefreshTargets bool
+		provider             fakeTargetProvider
+		currentTargetList    []string
+
+		currentHealthyTargets    []string
+		expectedHealthyTargets   []string
+		currentUnhealthyTargets  []string
+		expectedUnhealthyTargets []string
+
+		currentConsecutiveSuccessfulProbes  map[string]int
+		expectedConsecutiveSuccessfulProbes map[string]int
+		currentConsecutiveFailedProbes      map[string][]error
+		expectedConsecutiveFailedProbes     map[string][]error
+	}{
+		{
+			name:                   "a new target is not immediately added to the list of healthy targets",
+			shouldRefreshTargets:   true,
+			provider:               fakeTargetProvider{"master-0", "master-1", "master-2", "master-3"},
+			currentTargetList:      []string{"master-0", "master-1", "master-2"},
+			currentHealthyTargets:  []string{"master-0", "master-1", "master-2"},
+			expectedHealthyTargets: []string{"master-0", "master-1", "master-2"},
+		},
+
+		{
+			name:                                "an old target is immediately removed from the list of healthy targets",
+			shouldRefreshTargets:                true,
+			provider:                            fakeTargetProvider{"master-0", "master-1", "master-2"},
+			currentTargetList:                   []string{"master-0", "master-1", "master-2", "master-3"},
+			currentHealthyTargets:               []string{"master-0", "master-1", "master-2", "master-3"},
+			expectedHealthyTargets:              []string{"master-0", "master-1", "master-2"},
+			currentConsecutiveSuccessfulProbes:  map[string]int{"master-0": 3, "master-1": 3, "master-2": 3, "master-3": 3},
+			expectedConsecutiveSuccessfulProbes: map[string]int{"master-0": 3, "master-1": 3, "master-2": 3},
+		},
+
+		{
+			name:                                "an old target is immediately removed from the list of unhealthy targets",
+			shouldRefreshTargets:                true,
+			provider:                            fakeTargetProvider{"master-0", "master-1", "master-2"},
+			currentTargetList:                   []string{"master-0", "master-1", "master-2", "master-3"},
+			currentHealthyTargets:               []string{"master-0", "master-1", "master-2"},
+			expectedHealthyTargets:              []string{"master-0", "master-1", "master-2"},
+			currentUnhealthyTargets:             []string{"master-3"},
+			expectedUnhealthyTargets:            []string{},
+			currentConsecutiveFailedProbes:      map[string][]error{"master-3": []error{errors.New("abc")}},
+			expectedConsecutiveFailedProbes:     map[string][]error{},
+			currentConsecutiveSuccessfulProbes:  map[string]int{"master-0": 3, "master-1": 3, "master-2": 3},
+			expectedConsecutiveSuccessfulProbes: map[string]int{"master-0": 3, "master-1": 3, "master-2": 3},
+		},
+	}
+
+	for _, scenario := range scenarios {
+		t.Run(scenario.name, func(t *testing.T) {
+			monitor.refreshTargets = scenario.shouldRefreshTargets
+			monitor.targetProvider = scenario.provider
+			monitor.targetsToMonitor = scenario.currentTargetList
+			monitor.healthyTargets = scenario.currentHealthyTargets
+			monitor.unhealthyTargets = scenario.currentUnhealthyTargets
+			monitor.consecutiveSuccessfulProbes = scenario.currentConsecutiveSuccessfulProbes
+			monitor.consecutiveFailedProbes = scenario.currentConsecutiveFailedProbes
+
+			// act
+			monitor.refreshTargetsLocked()
+
+			// validate
+			if !cmp.Equal(monitor.healthyTargets, scenario.expectedHealthyTargets, cmpopts.EquateEmpty()) {
+				t.Errorf("unexpected list of healthy targets = %v, expected = %v", monitor.healthyTargets, scenario.expectedHealthyTargets)
+			}
+			if !cmp.Equal(monitor.unhealthyTargets, scenario.expectedUnhealthyTargets, cmpopts.EquateEmpty()) {
+				t.Errorf("unexpected list of unhealthy targets = %v, expected = %v", monitor.unhealthyTargets, scenario.expectedUnhealthyTargets)
+			}
+
+			if !cmp.Equal(monitor.consecutiveSuccessfulProbes, scenario.expectedConsecutiveSuccessfulProbes, cmpopts.EquateEmpty()) {
+				t.Errorf("unexpected state of consecutiveSuccessfulProbes = %v, expected = %v", monitor.consecutiveSuccessfulProbes, scenario.expectedConsecutiveSuccessfulProbes)
+			}
+			if !cmp.Equal(monitor.consecutiveFailedProbes, scenario.expectedConsecutiveFailedProbes, cmpopts.EquateEmpty()) {
+				t.Errorf("unexpected state of consecutiveFailedProbes = %v, expected = %v", monitor.consecutiveFailedProbes, scenario.expectedConsecutiveFailedProbes)
+			}
+		})
+	}
+}
+
+func TestRefreshTargets(t *testing.T) {
+	monitor := &healthMonitor{
+		unhealthyProbesThreshold: 2,
+		healthyProbesThreshold:   3,
+
+		consecutiveSuccessfulProbes: map[string]int{},
+		consecutiveFailedProbes:     map[string][]error{},
+	}
+
+	scenarios := []struct {
+		name                 string
+		shouldRefreshTargets bool
+		provider             fakeTargetProvider
+		currentTargetList    []string
+		expectedTargetList   []string
+	}{
+		{
+			name:               "shouldn't refresh, nothing changes",
+			currentTargetList:  []string{"master-0", "master-1", "master-2"},
+			expectedTargetList: []string{"master-0", "master-1", "master-2"},
+		},
+
+		{
+			name:               "new list available but hasn't been scheduled, nothing changes",
+			provider:           fakeTargetProvider{"master-0", "master-1", "master-2", "master-3"},
+			currentTargetList:  []string{"master-0", "master-1", "master-2"},
+			expectedTargetList: []string{"master-0", "master-1", "master-2"},
+		},
+
+		{
+			name:                 "new list available and scheduled, new target observed",
+			shouldRefreshTargets: true,
+			provider:             fakeTargetProvider{"master-0", "master-1", "master-2", "master-3"},
+			currentTargetList:    []string{"master-0", "master-1", "master-2"},
+			expectedTargetList:   []string{"master-0", "master-1", "master-2", "master-3"},
+		},
+
+		{
+			name:                 "new list available and scheduled, old target removed",
+			shouldRefreshTargets: true,
+			provider:             fakeTargetProvider{"master-0", "master-1", "master-2"},
+			currentTargetList:    []string{"master-0", "master-1", "master-2", "master-3"},
+			expectedTargetList:   []string{"master-0", "master-1", "master-2"},
+		},
+
+		{
+			name:                 "new list available and scheduled, old target removed and new one observed",
+			shouldRefreshTargets: true,
+			provider:             fakeTargetProvider{"master-0", "master-1", "master-2", "master-4"},
+			currentTargetList:    []string{"master-0", "master-1", "master-2", "master-3"},
+			expectedTargetList:   []string{"master-0", "master-1", "master-2", "master-4"},
+		},
+
+		{
+			name:                 "new list available and scheduled, all targets observed",
+			shouldRefreshTargets: true,
+			provider:             fakeTargetProvider{"master-0", "master-1", "master-2", "master-4"},
+			expectedTargetList:   []string{"master-0", "master-1", "master-2", "master-4"},
+		},
+
+		{
+			name:                 "new list available and scheduled, all targets removed",
+			shouldRefreshTargets: true,
+			provider:             fakeTargetProvider{},
+			currentTargetList:    []string{"master-0", "master-1", "master-2", "master-4"},
+			expectedTargetList:   []string{},
+		},
+	}
+
+	for _, scenario := range scenarios {
+		t.Run(scenario.name, func(t *testing.T) {
+			monitor.refreshTargets = scenario.shouldRefreshTargets
+			monitor.targetProvider = scenario.provider
+			monitor.targetsToMonitor = scenario.currentTargetList
+
+			// act
+			monitor.refreshTargetsLocked()
+
+			// validate
+			if !cmp.Equal(monitor.targetsToMonitor, scenario.expectedTargetList, cmpopts.EquateEmpty()) {
+				t.Errorf("unexpected list of targets = %v, expected = %v", monitor.targetsToMonitor, scenario.expectedTargetList)
+			}
+		})
+	}
+}
+
 func TestHealthProbes(t *testing.T) {
 	target := &healthMonitor{
 		targetsToMonitor:         []string{"master-0", "master-1", "master-2"},
@@ -82,9 +266,9 @@ func TestHealthProbes(t *testing.T) {
 		},
 
 		{
-			name:                     "round 9: master-1 became healthy",
-			currentHealthProbes:      []targetErrTuple{createHealthyProbe("master-0"), createHealthyProbe("master-1"), createHealthyProbe("master-2")},
-			expectedHealthyServers:   []string{"master-0", "master-1", "master-2"},
+			name:                   "round 9: master-1 became healthy",
+			currentHealthProbes:    []targetErrTuple{createHealthyProbe("master-0"), createHealthyProbe("master-1"), createHealthyProbe("master-2")},
+			expectedHealthyServers: []string{"master-0", "master-1", "master-2"},
 		},
 	}
 
