9b59ecd217c2bfffeae11c801e6514742b7e2143
Lukasz Szaszkiewicz <lukasz.szaszkiewicz@gmail.com>
2021-02-17 10:48:32 +0100

    update main

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"flag"
 	"fmt"
+	"k8s.io/apimachinery/pkg/util/sets"
 	"k8s.io/client-go/rest"
 	"net/http"
 	"time"
@@ -27,32 +28,45 @@ func main() {
 		panic(err.Error())
 	}
 	config.Timeout = 3 * time.Second
-	config.QPS = 1
-	config.Burst = 1
+	config.QPS = 100
+	config.Burst = 1000
 
 	config.TLSClientConfig.ServerName = "kubernetes.default.svc"
 
 	targetProvider := StaticTargetProvider{"10.0.139.201:6443", "10.0.154.83:6443", "10.0.165.154:6443"}
 	fmt.Println(fmt.Sprintf("creating and starting the health monitor for %v", targetProvider))
 	// TODO: copy the config and set a separate userAgent
-	hm, err := NewHealthMonitor(targetProvider, createConfigForHealthMonitor(config))
-	if err != nil {
-		panic(err)
-	}
-	go hm.StartMonitoring(context.TODO().Done())
-
-	loadBalancer, err := NewOxyRoundRobinLoadBalancer(hm)
+	healthMonitor, err := NewHealthMonitor(targetProvider, createConfigForHealthMonitor(config))
 	if err != nil {
 		panic(err)
 	}
 
-	config.Wrap(newCustomTransportWrapper(loadBalancer))
+	loadBalancer, err := NewOxyRoundRobinLoadBalancer()
+	if err != nil {
+		panic(err)
+	}
+
+	// I think that the following setting will trigger the circuit breaker:
+	//   when 1% of the requests returned a 5XX status
+	//   OR when the ratio of network errors reaches 1%,
+	//     it looks that StatusGatewayTimeout OR StatusBadGateway HTTP status codes
+	//     are considered network errors (https://github.com/p0lyn0mial/oxy/blob/c52e3e446ec6a8a2ee1fc7a9cc85a06e999b05ca/memmetrics/roundtrip.go#L212)
+	//
+	// TODO: set fallback duration and recovery duration
+	circuitBreakerFn := NewOxyCircuitBreakerWithOptions("ResponseCodeRatio(500, 600, 0, 600) > 0.1 || NetworkErrorRatio() > 0.1")
+	circuitBreakerFactory := NewOxyCircuitBreakerFactory(healthMonitor, loadBalancer, circuitBreakerFn)
+
+	ct := newCustomTransportWrapper(loadBalancer, circuitBreakerFactory.Get)
+	config.Wrap(ct)
 
 	fmt.Println("creating the k8s client set for the config")
 	clientset, err := kubernetes.NewForConfig(config)
 	if err != nil {
 		panic(err.Error())
 	}
+
+	go healthMonitor.StartMonitoring(context.TODO().Done())
+	// TODO wait for healthy servers to be observed
 
 	for {
 		fmt.Println("about to LIST secrets in the test-01 namespace")
@@ -62,7 +76,6 @@ func main() {
 		}
 		fmt.Println(fmt.Sprintf("found %d secrets in the default namespace", len(ret.Items)))
 		fmt.Println("")
-		time.Sleep(35 * time.Second)
 	}
 }
 
@@ -78,55 +91,55 @@ func createConfigForHealthMonitor(restConfig *rest.Config) *rest.Config {
 	return &restConfigCopy
 }
 
-func newCustomTransportWrapper(lb LoadBalancer) func(http.RoundTripper) http.RoundTripper {
-	return func(rt http.RoundTripper) http.RoundTripper { return &customTransport{baseRT: rt, lb: lb} }
+func newCustomTransportWrapper(lb LoadBalancer, cbGetter func(server string) CircuitBreaker) func(http.RoundTripper) http.RoundTripper {
+	return func(rt http.RoundTripper) http.RoundTripper { return &customTransport{baseRT: rt, lb: lb, cbGetter: cbGetter} }
 }
 
 type customTransport struct {
 	baseRT http.RoundTripper
 	lb     LoadBalancer
+	cbGetter func(server string) CircuitBreaker
 }
 
 func (t *customTransport) RoundTrip(r *http.Request) (*http.Response, error) {
 	fmt.Println(fmt.Sprintf("customTransport: RoundTrip: received a request for Method = %q to Host = %q, URL = %q", r.Method, r.Host, r.URL))
 
+	// TODO: filter on specific path (for example only SAR requests)
 	// TODO: if there are no healthy targets we could use the service IP - why not ?
 	server, err := t.lb.NextServer()
 	if err != nil {
 		return nil, err
 	}
+	seenServersSet := sets.NewString(server)
 
-	// I don't like modifying the original request
-	r.Host = server
-	r.URL.Host = server
+	for {
+		// I don't like modifying the original request
+		r.Host = server
+		r.URL.Host = server
 
-	fmt.Println(fmt.Sprintf("customTransport: RoundTrip: The new Host is = %q", r.Host))
+		fmt.Println(fmt.Sprintf("customTransport: RoundTrip: The new Host is = %q", r.Host))
 
-	// TODO: needs proper implementation
-	rt := getCircuitBreakerForEndpoint(server, t.baseRT)
-	return rt.RoundTrip(r)
-}
+		crt := t.cbGetter(server)
+		rsp, err := crt.RoundTripWithDelegate(r, t.baseRT)
+		if rsp == nil && err != nil && crt.FailedBecauseCircuitBreakerIsOpen(err) {
+			allServers := t.lb.Servers()
+			potentialServersSet := sets.NewString(allServers...)
+			potentialServersSet.Delete(seenServersSet.List()...)
+			if potentialServersSet.Len() == 0 {
+				return nil, fmt.Errorf("service unavailable, tried %d servers", seenServersSet.Len())
+			}
+			server = potentialServersSet.List()[0]
+			seenServersSet.Insert(server)
+			continue
+		}
 
-// getCircuitBreakerForEnpoint could map an endpoint (URL) to a circuit breaker aware endpoint
-// the mapper could have a cache (LRU) to store the mapping
-func getCircuitBreakerForEndpoint(ep string, delegate http.RoundTripper) http.RoundTripper {
-	return &cb{delegate: delegate}
-}
+		if seenServersSet.Len() > 1 {
+			// this is the place where we are going to measure success of this mechanism
+			// we have hit more than one server
+			// if the request succeeded (HTTP 200) we have won
+			// TODO: add a metric
+		}
 
-type cb struct {
-	delegate http.RoundTripper
-}
-
-// the CB aware endpoint could:
-//  - be safe for concurrent use just like http.Client is
-//  - implement RoundTrip interface
-//  - analyse err
-//  - analyse rsp (for example HTTP 500)
-//  - analyse latency
-//  - update its internal state
-//  - notify the load balancer
-func (c *cb) RoundTrip(r *http.Request) (*http.Response, error) {
-	fmt.Println(fmt.Sprintf("circuitBreaker: RoundTrip: received a request for Method = %q to Host = %q, URL = %q", r.Method, r.Host, r.URL))
-	defer fmt.Println(fmt.Sprintf("circuitBreaker: RoundTrip ended for Host = %q", r.Host))
-	return c.delegate.RoundTrip(r)
+		return rsp, err
+	}
 }
